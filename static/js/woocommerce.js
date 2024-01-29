c2bd6a28f30f1b4b16f288244ce997211cb03a15
Branko Tosin <branko@citizenshipper.com>
2024-01-29 10:59:34 +0100

    Add initial woocommerce styling

--- static/js/woocommerce.js
+++ static/js/woocommerce.js
@@ -1,7 +1,28 @@
 jQuery(document).ready(function () {
-  jQuery(".filter--style-premmerce .filter__title").on("click", function () {
+  jQuery(".filter__header .filter__title").on("click", function () {
     const $filterItem = jQuery(this).closest(".filter__item");
 
     $filterItem.find(".filter__properties-list").toggleClass("active");
   });
+
+  const $filtersContainer = jQuery(".premmerce-filter-ajax-container");
+
+  const $filters = jQuery(".widget_premmerce_filter_filter_widget");
+
+  const $filterProducts = jQuery(
+    `<button class="filter-products">Show filters</button>`
+  );
+
+  $filterProducts.on("click", function () {
+    const $button = jQuery(this);
+    if ($filters.hasClass("show")) {
+      $filters.removeClass("show");
+      $button.text("Show filters");
+    } else {
+      $filters.addClass("show");
+      $button.text("Hide filters");
+    }
+  });
+
+  $filtersContainer.prepend($filterProducts);
 });
